6a4d4b7fa37e28e1a4596bbc3620e677e814b996
Miho Wakai <miho.wakai@desy.de>
2018-10-31 11:55:37 +0000

    added trigger

--- triggers.C
+++ triggers.C
@@ -54,25 +54,25 @@ void triggers(char *filename=NULL) {
   cout << "Nb of entries " << wcsimT->GetEntries() << endl;
   //-----------------------
 
-  TH2D *TrigvsEvent = new TH2D("TrigvsEvent","Number of Triggers vs. Events", 100, 0, 15, 10, 0, 30);
-  TrigvsEvent->SetYTitle("Number of Triggers");
-  TrigvsEvent->SetXTitle("Event Number");
+  // TH2D *TrigvsEvent = new TH2D("TrigvsEvent","Number of Triggers vs. Events", 100, 0, 15, 10, 0, 30);
+  // TrigvsEvent->SetYTitle("Number of Triggers");
+  // TrigvsEvent->SetXTitle("Event Number");
 
   const long unsigned int nbEntries = wcsimT->GetEntries();
 
   for(long unsigned int iEntry = 0; iEntry < nbEntries; iEntry++){
     // Point to event iEntry inside WCSimTree
     wcsimT->GetEvent(iEntry);
-
+    cout << "iEntry " << iEntry << endl;
     // Nb of Trigger inside the event
     const unsigned int nbTriggers = wcsimroothyperevent->GetNumberOfEvents();
     const unsigned int nbSubTriggers = wcsimroothyperevent->GetNumberOfSubEvents();
 
     for(long unsigned int iTrig = 0; iTrig < nbTriggers; iTrig++){
       WCSimRootTrigger *wcsimrootevent = wcsimroothyperevent->GetTrigger(iTrig);
-      double num = iEntry;
-      double trig = wcsimroothyperevent->GetTrigger(iTrig);
-      TrigvsEvent->Fill(num, trig);
+      cout << "Number of Triggers " << wcsimroothyperevent->GetTrigger(iTrig) << endl;
+
+      // TrigvsEvent->Fill(num, trig);
 
 
     } // END FOR iTRIG
@@ -80,8 +80,8 @@ void triggers(char *filename=NULL) {
   } // END FOR iENTRY
 
 
-    TH1 *temp;
-    TCanvas *c1 = new TCanvas("c1","c1", 700, 700);
-    TrigvsEvent->Draw("colz");
+    // TH1 *temp;
+    // TCanvas *c1 = new TCanvas("c1","c1", 700, 700);
+    // TrigvsEvent->Draw("colz");
 
   }
