0b7df8d9664de428b9b8df58f8ed5023768a2919
Miho Wakai <miho.wakai@desy.de>
2018-10-31 12:41:36 +0000

    added trigger file

--- triggers.C
+++ triggers.C
@@ -54,9 +54,9 @@ void triggers(char *filename=NULL) {
   cout << "Nb of entries " << wcsimT->GetEntries() << endl;
   //-----------------------
 
-  // TH2D *TrigvsEvent = new TH2D("TrigvsEvent","Number of Triggers vs. Events", 100, 0, 15, 10, 0, 30);
-  // TrigvsEvent->SetYTitle("Number of Triggers");
-  // TrigvsEvent->SetXTitle("Event Number");
+  TH2D *TrigvsEvent = new TH2D("TrigvsEvent","Number of Triggers vs. Events", 100, 0, 15, 10, 0, 30);
+  TrigvsEvent->SetYTitle("Number of Triggers");
+  TrigvsEvent->SetXTitle("Event Number");
 
   const long unsigned int nbEntries = wcsimT->GetEntries();
 
@@ -67,23 +67,23 @@ void triggers(char *filename=NULL) {
     // Nb of Trigger inside the event
     const unsigned int nbTriggers = wcsimroothyperevent->GetNumberOfEvents();
     const unsigned int nbSubTriggers = wcsimroothyperevent->GetNumberOfSubEvents();
-    cout << "GetNumberofEvents " << nbTriggers << endl;
-    cout << "GetNumberOfSubEvents " << nbSubTriggers << endl;
+    // cout << "GetNumberofEvents " << nbTriggers << endl;
+    // cout << "GetNumberOfSubEvents " << nbSubTriggers << endl;
 
-    for(long unsigned int iTrig = 0; iTrig < nbTriggers; iTrig++){
-      WCSimRootTrigger *wcsimrootevent = wcsimroothyperevent->GetTrigger(iTrig);
-      cout << "Number of Triggers " << wcsimroothyperevent->GetTrigger(iTrig) << endl;
-
-      // TrigvsEvent->Fill(num, trig);
-
-
-    } // END FOR iTRIG
+    TrigvsEvent->Fill(iEntry, nbTriggers);
+    // for(long unsigned int iTrig = 0; iTrig < nbTriggers; iTrig++){
+    //   WCSimRootTrigger *wcsimrootevent = wcsimroothyperevent->GetTrigger(iTrig);
+    //
+    //   // TrigvsEvent->Fill(num, trig);
+    //
+    //
+    // } // END FOR iTRIG
 
   } // END FOR iENTRY
 
 
-    // TH1 *temp;
-    // TCanvas *c1 = new TCanvas("c1","c1", 700, 700);
-    // TrigvsEvent->Draw("colz");
+    TH1 *temp;
+    TCanvas *c1 = new TCanvas("c1","c1", 700, 700);
+    TrigvsEvent->Draw("colz");
 
   }
